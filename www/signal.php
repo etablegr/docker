cb9cb4dce8016cbbe67af2a506aae1c301518a66
Dimitrios Desyllas <pc-magas@users.noreply.github.com>
2020-01-23 13:31:06 +0200

    ETABLEWEB-1026 Build docker supervisor.

--- www/signal.php
+++ www/signal.php
@@ -1,12 +1,9 @@
 <?php
 
-define('LOGFILE',__DIR__.'/signal_'.getmypid().'.log');
-
 function shutdown()
 {
     $date=(new DateTime())->format("%Y-%m-%d %H:%i:%s");
     echo "[${date}] received kill signal".PHP_EOL;
-    file_put_contents(LOGFILE,"[${date}] received kill signal".PHP_EOL,FILE_APPEND);
     exit(1);
 }
 
@@ -19,12 +16,10 @@ pcntl_signal(SIGCHLD, 'shutdown');
 
 $date=(new DateTime())->format("%Y-%m-%d %H:%i:%s");
 echo "[${date}] started".PHP_EOL;
-file_put_contents(LOGFILE, "[${date}] started".PHP_EOL,FILE_APPEND);
 
 while(true){
     $date=(new DateTime())->format("%Y-%m-%d %H:%i:%s");
     echo "[${date}] Doing Stuff".PHP_EOL;
-    file_put_contents(LOGFILE,"[${date}] Doing Stuff".PHP_EOL,FILE_APPEND);
     sleep(1);
 }
 
