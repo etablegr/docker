a22e22acb50ba90abe4d72cd26fd7be2ad2280fb
Dimitrios <pcmagas@disroot.org>
2020-05-18 13:26:52 +0300

    ETABLEWEB-1272 Test that composer is installed.

---
+++ tests/test_composer.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+echo "Testing whether composer exists"
+
+declare -a images=( 'etable/php7.2-dev' 'etable/php7.2' 'etable/php7.0.8' )
+
+for image in "${images[@]}"; do
+ container_name="test_composer_"$(echo $image | tr \/ _ )
+ docker run --name $container_name $image composer --version
+ sleep 1
+ status=$(docker inspect $container_name --format='{{.State.ExitCode}}')
+ docker rm $container_name
+
+ echo $image' ==> '$status
+ if [ "$status" != "0" ]; then
+    echo "Composer not Installed on image ${image}"
+    exit $status
+ fi
+done;
